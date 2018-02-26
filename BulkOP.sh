41272023ca27fe249771b104c22f8ed26e56cf23
Sky Moore <mskymoore@gmail.com>
2018-02-25 19:06:58 -0800

    Added BulkOP.sh and BulkSCP.sh

---
+++ BulkOP.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+
+# Title:	testSSHConnection.sh
+# Author:       Sky Moore (mskymoore@gmail.com)
+# Summary:      Attempts to log in to ssh servers with username@hostname lines read from input file, and reports to the console the result of the attempt.
+#               
+# Options:      [-i /file/to/read/from]
+# Exit Codes:   0: successful execution
+#               1: help printed or bad arguments
+#               2: 
+#               3: 
+
+usage() { echo "$0 usage:" && grep ".)\ #" $0; exit 1; }
+
+
+[ $# -eq 0 ] && usage
+
+while getopts ":hH:o:" arg;do
+	case $arg in
+		H) # Hosts file to read username@hostname lines from
+			HostsFile=$(readlink -f ${OPTARG})
+			;;
+		o) # Operation to perform in bulk on remote hosts
+			OP=${OPTARG}
+			;;
+		#p)## Request a password to log in to all username@hostname with
+		#	read -s -p "Password: " Password	
+		#	;;
+		h | *) # Display this help message
+			usage
+			;;
+	esac
+done
+
+if [[ -z $HostsFile ]];then
+	echo "You must supply an input file."
+	usage
+fi
+
+if [[ -e $HostsFile ]];then		
+	IFS=$'\n' read -d '' -r -a lines < $HostsFile
+	for aHost in "${lines[@]}"
+	do
+		status=$(ssh -o BatchMode=yes -o ConnectTimeout=15 $aHost echo ok 2>&1)
+		case $status in
+			ok) 
+				echo "Connected to $aHost successfully."
+				echo "Attempting to execute $OP on $aHost"
+				if ssh -o ConnectTimeout=10 $aHost $OP;then
+					echo "Success!"
+				else
+					echo "Failure."
+				fi	
+			;;
+			*"Permission denied"*) 
+				echo "Failed to connect to $aHost."
+			;;
+			*) 
+				echo "$aHost status: $status."
+			;;
+		esac
+					
+	done	
+else
+	echo "$HostsFile is invalid or does not exist."
+	exit 1
+fi
+exit 0
---
+++ BulkSCP.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+
+# Title:	testSSHConnection.sh
+# Author:       Sky Moore (mskymoore@gmail.com)
+# Summary:      Attempts to log in to ssh servers with username@hostname lines read from input file, and reports to the console the result of the attempt.
+#               
+# Options:      [-i /file/to/read/from]
+# Exit Codes:   0: successful execution
+#               1: help printed or bad arguments
+#               2: 
+#               3: 
+
+usage() { echo "$0 usage:" && grep ".)\ #" $0; exit 1; }
+
+
+[ $# -eq 0 ] && usage
+
+while getopts ":hH:i:r:" arg;do
+	case $arg in
+		H) # Hosts file to read username@hostname lines from
+			HostsFile=$(readlink -f ${OPTARG})
+			;;
+		i) # Input file or directory to copy to remote hosts
+			InputFile=$(readlink -f ${OPTARG})
+			;;
+		r) # Remote directory to copy Input file or directory to on hosts
+			RemotePath=${OPTARG}
+			;;
+		#p)## Request a password to log in to all username@hostname with
+		#	read -s -p "Password: " Password	
+		#	;;
+		h | *) # Display this help message
+			usage
+			;;
+	esac
+done
+
+if [[ -z $HostsFile ]];then
+	echo "You must supply an input file."
+	usage
+fi
+
+if [[ -e $HostsFile ]];then
+	if [[ -e $InputFile ]];then
+		IFS=$'\n' read -d '' -r -a lines < $HostsFile
+		for aHost in "${lines[@]}"
+		do	
+			status=$(ssh -o BatchMode=yes -o ConnectTimeout=15 $aHost echo ok 2>&1)
+			case $status in
+				ok) 
+					echo "Connected to $aHost successfully."
+					echo "Checking for $RemotePath on $aHost..."
+					if ssh $aHost [[ -e $RemotePath  ]];then	
+						# if InputFile is a directory
+						if [[ -d $InputFile ]];then
+							echo "Transferring directory $InputFile to $aHost:$RemotePath"
+							# call scp with -r option for recursively copying directory contents
+							scp -r $InputFile $aHost:$RemotePath
+							echo "Transfer complete."	
+						# elif InputFile is a regular file
+						elif [[ -f $InputFile ]];then
+							echo "Transferring file $InputFile to $aHost:$RemotePath"
+							# call scp with no options
+							scp $InputFile $aHost:$RemotePath
+							echo "Transfer complete."						
+						# else InputFile is something else, this statement should never be reached
+						else
+							echo "Local path entered is invalid or does not exist."		
+							exit 2
+						fi
+					else
+						echo "$RemotePath is invalid, does not exist on $aHost."
+					fi
+				;;
+				*"Permission denied"*) 
+					echo "Failed to connect to $aHost."
+				;;
+				*) 
+					echo "$aHost status: $status."
+				;;
+			esac
+		done
+	else
+		echo "$InputFile is invalid or does not exist."
+	fi		
+else
+	echo "$HostsFile is invalid or does not exist."
+	exit 1
+fi
+exit 0
